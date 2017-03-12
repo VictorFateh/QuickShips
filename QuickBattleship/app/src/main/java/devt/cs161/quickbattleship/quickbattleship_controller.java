67ddf6c91f831377a4e280dc552ca809e626c262
Trinh Nguyen <trinh.d.nguyen@sjsu.edu>
2017-03-12 00:56:12 -0800

    basic starting codes

---
+++ QuickBattleship/app/src/main/java/devt/cs161/quickbattleship/quickbattleship_controller.java
@@ -0,0 +1,13 @@
+package devt.cs161.quickbattleship;
+
+/**
+ * Created by trinhnguyen on 3/12/17.
+ */
+
+public class quickbattleship_controller {
+    quickbattleship_model model;
+
+    public quickbattleship_controller() {
+        model = new quickbattleship_model();
+    }
+}
