dc3ab3272c439e55c5a45dd53869ba97ad3fccfa
Trinh Nguyen <trinh.d.nguyen@sjsu.edu>
2017-03-14 14:56:53 -0700

    canvas view

--- QuickShip/app/src/main/java/dev_t/cs161/quickship/quickShipController.java
+++ QuickShip/app/src/main/java/dev_t/cs161/quickship/quickShipController.java
@@ -1,11 +1,21 @@
 package dev_t.cs161.quickship;
 
-import android.util.Log;
-import java.util.UUID;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
 
-public class quickShipController {
+public class quickShipController extends Activity {
 
-    public quickShipController() {
+    private quickShipView _mainView;
+    private quickShipController _mainController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _mainController = new quickShipController();
+        _mainView = (quickShipView) findViewById(R.id.main_screen);
         quickShipModel player1 = new quickShipModel();
         quickShipModel player2 = new quickShipModel();
         quickShipBoard player1Board = player1.getPlayerGameBoard();
@@ -15,4 +25,9 @@ public class quickShipController {
         player1.printMap_debug();
         player2.printMap_debug();
     }
+
+    public void clearCanvas(View v) {
+        _mainView.clearCanvas();
+    }
+
 }
