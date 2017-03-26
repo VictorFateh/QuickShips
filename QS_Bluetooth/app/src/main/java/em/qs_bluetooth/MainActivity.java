c49eb285aa839de30ff67bb598c30aa27a89a4e1
emmendoza1986 <emmendoza1986@gmail.com>
2017-03-26 02:49:45 -0700

    tiTkered some more with bluetooth. Still more work to be done.

--- QS_Bluetooth/app/src/main/java/em/qs_bluetooth/MainActivity.java
+++ QS_Bluetooth/app/src/main/java/em/qs_bluetooth/MainActivity.java
@@ -1,12 +1,18 @@
 package em.qs_bluetooth;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.content.Intent;
+import android.content.IntentFilter;
+
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -67,6 +73,8 @@ public class MainActivity extends AppCompatActivity {
                     tv_status.setText("Bluetooth is now off.");
                     btn_disconnect.setVisibility(View.GONE);
                     btn_connect.setVisibility(View.VISIBLE);
+                    btn_hostGame.setVisibility(View.VISIBLE);
+                    btn_joinGame.setVisibility(View.VISIBLE);
                 }
             }
         });
@@ -79,18 +87,60 @@ public class MainActivity extends AppCompatActivity {
                 //discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
                 tv_status.setText("Attempting to Host Game...");
                 btn_joinGame.setVisibility(View.GONE);
-                startActivity(discoverableIntent);
+                //startActivity(discoverableIntent);
+                startActivityForResult(discoverableIntent, 1);
             }
         });
 
         btn_joinGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(btAdapter == null) {
+                    btAdapter = BluetoothAdapter.getDefaultAdapter();
+                    if( ! btAdapter.isEnabled()) {
+                        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        int REQUEST_ENABLE_BT = 1;
+                        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                    }
+                }
+
+                if(btAdapter.isDiscovering())
+                    btAdapter.cancelDiscovery();
                 //TODO connect to device in discovery mode
+                btAdapter.startDiscovery();
+                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                registerReceiver(mReceiver, filter);
+
             }
         });
 
         //TODO once devices are connected, start threads to allow data to flow between devices.
     }
 
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if(btAdapter != null)
+            btAdapter.cancelDiscovery();
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(mReceiver);
+    }
 }
